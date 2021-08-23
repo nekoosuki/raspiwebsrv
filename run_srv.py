2ccb2ff6dc5866258250d23becb5e6e4899bd48e
nekoosuki <1356812370@qq.com>
2021-08-23 22:14:30 +0800

    argupdate

--- run_srv.py
+++ run_srv.py
@@ -2,10 +2,13 @@
 # -*- coding: UTF-8 -*-
 
 from http.server import HTTPServer, CGIHTTPRequestHandler
+import argparse
 
-port = 8081
-httpd = HTTPServer(('', port), CGIHTTPRequestHandler)
-print(f'starting httpd on port {port}')
+parser = argparse.ArgumentParser('python run_srv.py')
+parser.add_argument('-p', '--port', type=int, default=80, help='port which server is running on')
+args = parser.parse_args()
+httpd = HTTPServer(('', args.port), CGIHTTPRequestHandler)
+print(f'starting httpd on port {args.port}')
 try:
     httpd.serve_forever()
 except KeyboardInterrupt:
