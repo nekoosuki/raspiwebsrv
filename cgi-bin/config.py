3b50237ccf8b4ed672782b10b5fed5cbfb7b8abc
nekoosuki <1356812370@qq.com>
2021-08-24 14:32:01 +0800

    1.2

--- cgi-bin/config.py
+++ cgi-bin/config.py
@@ -10,12 +10,38 @@ iou = eval(form.getvalue("iou"))
 state = eval(form.getvalue("state"))
 sql = eval(form.getvalue("sql"))
 
-if sql:
-    with open('json/dev_info.json', 'r') as f:
-        ip = json.load(f)['srv_ip']
-    # TODO 提交数据库
 data = {"conf": conf, "iou": iou, "state": state}
-with open('json/config.json', 'w') as f:
-    json.dump(data, f, indent=4)
+if sql:
+    try:
+        with open('json/dev_info.json', 'r') as f:
+                ip = json.load(f)['srv_ip']
+            # TODO 提交数据库
+    except:
+        # 提交数据库失败
+        data.update({'syn': -1})
+        # 回滚
+    else:
+        # 提交数据库成功
+        data.update({'syn': 1})
+else:
+    # 未提交数据库
+    data.update({'syn': 0})
+
+try:
+    with open('json/config.json', 'w') as f:
+            json.dump(data, f, indent=4)
+except:
+    # 提交本地失败
+    data.update({'local': -1})
+    if data.get('syn') == 1:
+        # 回滚
+        pass
+else:
+    # 提交本地成功
+    data.update({'local': 1})
+finally:
+    #关闭游标和连接
+    pass
+
 print("Content-type: application/json\n")
 print(json.dumps(data))
